6d5f879b1ec6df9323c4dfa7f2d16fe1fd350581
Zhang Qi <zhangqi@tr-ag.com>
2024-02-09 00:03:00 +0800

    hotfix-函数优化/1.4.0

--- components/the_nginx_forwarder.sh
+++ components/the_nginx_forwarder.sh
@@ -22,24 +22,66 @@ the_nginx_forwarder() {
   echo "🚩upstream--请输入后端服务地址"
   read upstream
   echo -e "\033[0m"
-  local this_port=$port
-  if [ -z "$(netstat -tunlp | grep $this_port)" ]; then
-    # echo "端口：$this_port 未被占用"
-  else
-    #echo "端口：$this_port 已被占用"
+  the_port_checker $port
+  if [ $? -ne 0 ]; then
+    echo "端口：$port 已被占用"
     exit 1
   fi
-  #遍历conf.d目录下的配置文件，了解端口占用情况
-  for file in $(ls $the_nginx_conf_dir); do
-    if [ "${file##*.}" != "conf" ]; then
-      continue
-    fi #如果文件不是以.conf结尾，则跳过
-    nginx_port=$(cat $the_nginx_conf_dir/$file | grep "listen" | awk '{print $2}' | awk -F ";" '{print $1}')
-    if [ $this_port -eq $nginx_port ]; then
-      #echo "端口：$this_port 已被占用"
-      exit 1
-    fi
-  done
+  #从env中读取server_name，wwwroot
+  #例 server_name=www.example.com
+  #   wwwroot=/var/www/html
+  source $the_nginx_env
+  echo "当前变量："
+  echo "server_name=$server_name"
+  echo "wwwroot=$wwwroot"
+  echo "server {
+	# 域名	
+	server_name $server_name;
+	
+	# 端口
+	listen $port ssl http2;
+	
+	#证书文件
+	ssl_certificate /etc/ssl/certs/$server_name.crt; 
+
+	#私钥文件
+	ssl_certificate_key /etc/ssl/keys/$server_name.key; 
+	ssl_session_timeout 5m;
+
+	#加密协议
+	ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
+
+	#加密套件
+	ssl_ciphers ECDHE-RSA-AES128-GCM-SHA256:HIGH:!aNULL:!MD5:!RC4:!DHE; 
+	ssl_prefer_server_ciphers on;
+
+	# 网站目录
+	root $wwwroot;
+
+	# 网站主页
+	index index.html index.htm index.php;
+
+	 location / {
+			proxy_set_header X-Real-IP \$remote_addr;
+			proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+			proxy_set_header Host \$http_host;
+			proxy_set_header X-NginX-Proxy true;
+			proxy_set_header  X-Forwarded-Proto \$scheme;
+			proxy_pass $upstream;
+			proxy_redirect off;
+
+			#WebSocket设置
+			 proxy_read_timeout 300s;
+			 proxy_send_timeout 300s;
+
+			 proxy_http_version 1.1;
+			 proxy_set_header Upgrade \$http_upgrade;
+			 proxy_set_header Connection \$connection_upgrade;
+	 }
+
+	#  隐藏nginx版本号
+	server_tokens off;
+}" >$the_nginx_conf_dir/$port.conf
 }
 
 the_environment_checker() {
@@ -53,4 +95,19 @@ the_environment_checker() {
     exit 1
   fi
   echo "nginx命令以及配置目录存在"
+}
+
+the_port_checker() {
+  this_port=$1
+  #遍历conf.d目录下的配置文件，了解端口占用情况
+  for file in $(ls $the_nginx_conf_dir); do
+    if [ "${file##*.}" != "conf" ]; then
+      continue
+    fi #如果文件不是以.conf结尾，则跳过
+    nginx_port=$(cat $the_nginx_conf_dir/$file | grep "listen" | awk '{print $2}' | awk -F ";" '{print $1}')
+    if [ $this_port -eq $nginx_port ]; then
+      #echo "端口：$this_port 已被占用"
+      exit 1
+    fi
+  done
 }
