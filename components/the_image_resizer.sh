cdbdc48a383d8988797cdf3d77a63eb9ad1b239a
Zhang Qi <zhangqi@tr-ag.com>
2024-02-08 22:34:17 +0800

    hotfix-继续尝试自动化构建/1.1.8

--- components/the_image_resizer.sh
+++ components/the_image_resizer.sh
@@ -13,7 +13,8 @@ the_image_resizer() {
   echo "先将原版的"$img_file"解压"
   gzip -d $img_file
   echo "然后扩容至约384M"
-  dd if=/dev/zero bs=4096k count=96 >>$img_file
+  dd if=/dev/zero bs=4096k count=256 >>$img_file
+  img_file=${img_file%.*} #去掉后缀
   echo "观察文件大小是否变化"
   ls -lh $img_file
 
