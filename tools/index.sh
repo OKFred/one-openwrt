4b9bfaf6f284b8f8e223326ab04322adcc60a589
okfred <zq.admin.vip@gmail.com>
2026-06-29 00:02:47 +0800

    feat: 延时启动

--- tools/index.sh
+++ tools/index.sh
@@ -51,8 +51,8 @@ read -r choice
 case "$choice" in
     [yY][eE][sS]|[yY])
         echo "[INFO] Config start on boot--正在配置开机自启..."
-        add_to_rc_local "(cd $SCRIPT_DIR/og && sh ./index.sh)"
-        add_to_rc_local "(cd $SCRIPT_DIR/pw && sh ./index.sh)"
+        add_to_rc_local "(sleep 60 && cd $SCRIPT_DIR/og && sh ./index.sh) &"
+        add_to_rc_local "(sleep 60 && cd $SCRIPT_DIR/pw && sh ./index.sh) &"
         ;;
     *)
         echo "[INFO] Skip start on boot--跳过配置开机自启，直接在当前终端启动服务..."
